740a520186be395b70451ba65e5963552106f527
RoopeKatisko <kristofer.katisko@gmail.com>
2026-02-11 17:02:42 +0100

    Added health, and habitat attributes

--- evolutiongame1/Evolutiongame.py
+++ evolutiongame1/Evolutiongame.py
@@ -1,5 +1,5 @@
 class Animal: #parent class
-    def __init__(self, domain, kingdom, phylum, clas, order, family, genus, species):
+    def __init__(self, domain, kingdom, phylum, clas, order, family, genus, species, health):
         self.domain = domain
         self.kingdom = kingdom
         self.phylum = phylum
@@ -8,6 +8,9 @@ class Animal: #parent class
         self.family = family
         self.genus = genus
         self.species = species
+        self.health = initial_health
+        initial_health = 100
+
 
 class terrestial(Animal): #subclass 1):
     def __init__(self, domain, kingdom, phylum, clas, order, family, genus, species, lungs, fur, colour, limbs):
@@ -37,5 +40,12 @@ class Habitat:
         self.elevation = elevation
         self.terrestial = terrestial
         self.aqeous = aqeous
-    
-      
+#humidity goes from 0/100 and elevation/temperature and terrestrial/aqeuos are 0/100 with respect to eachother are real numbers
+Desert.habitat = Habitat(temperature = 40, humidity = 10, elevation = 20, terrestrial = 99, aqeous=1)
+Ocean.habitat = Habitat(temperature = 10, humidity = 100, elevation = 0, terrestrial = 0, aqeous=100)
+Forest.habitat = Habitat(temperature = 15, humidity = 40, elevation = 100, terrestrial = 75, aqeous=25)
+Mountains.habitat = Habitat(temperature = -10, humidity = 60, elevation = 3000, terrestrial = 90, aqeous=10)
+Lake.habitat = Habitat(temperature = 10, humidity = 100, elevation = 0, terrestrial = 0, aqeous=100)
+shore.habitat = Habitat(temperature = 15, humidity = 80, elevation = 0, terrestrial = 40, aqeous=60)
+
+
